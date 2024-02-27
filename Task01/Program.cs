2fdc7566888deabdde0df782097404e40b459c57
Sergei Klokov <s2584835@ya.ru>
2024-02-27 20:32:30 +0300

    feat: solve chapter07 task01

---
+++ Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, в которой есть статический метод. Аргументом методу передается текстовое значение.
+// Результатом метод возвращает текст, в котором, по сравнению с текстом-аргументом, между символами вставлены пробелы.
+
+static string AddSpace(string txt)
+{
+    string newText = "";
+    foreach(char i in txt)
+    {
+        newText += i + " ";
+    }
+    return newText.TrimEnd();
+}
+
+Console.WriteLine("Введите исходный текст:");
+string text = Console.ReadLine();
+string newText = AddSpace(text);
+Console.WriteLine(newText);
