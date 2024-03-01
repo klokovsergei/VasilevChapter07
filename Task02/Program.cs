955fcce07d07e6d4fc723d93dd1eeebea9f4b6b8
Sergei Klokov <s2584835@ya.ru>
2024-03-01 18:51:05 +0300

    feat: solve task02

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, в которой есть статический метод, возвращаю­щий результатом текстовое значение
+// и получающий аргументом текст. Результат метода - это переданный аргументом текст, в котором слова следуют
+// в обратном порядке. Словами считать блоки текста, разделен­ные пробелами.
+
+static string ReversText(string txt)
+{
+    string[] textArray = txt.Split(" ");
+    string newText = "";
+    foreach(string t in textArray)
+    {
+        newText = t + " " + newText;
+    }
+    return newText;
+}
+
+string txt = "один два три четыре";
+Console.WriteLine(ReversText(txt));
